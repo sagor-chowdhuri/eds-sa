1bb6641b7cf1b7409660f08ebcfed0871e61e83b
SaiManikantha <ssanagavarpu@deloitte.com>
2025-03-23 13:38:36 +0530

    commit

--- blocks/democomp/democomp.js
+++ blocks/democomp/democomp.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
     const h1 = document.createElement('h1');
     h1.textContent = 'Hello World';
-    block.innerHTML = '';
+    block.innerHTML = "";
     block.append(h1);
 }
