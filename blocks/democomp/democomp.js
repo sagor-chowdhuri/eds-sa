a5e011cd93109e6625073cc32436c053e224b719
SaiManikantha <ssanagavarpu@deloitte.com>
2025-03-23 13:21:42 +0530

    commit

--- blocks/democomp/democomp.js
+++ blocks/democomp/democomp.js
@@ -1,11 +1,14 @@
 export default function decorate(block) {
-    const cols = [...block.firstElementChild.children];
-     
-    // setup image columns
+    /* change to ul, li */
+    const ul = document.createElement('ul');
     [...block.children].forEach((row) => {
-      [...row.children].forEach((col) => {
-        const pic = col.querySelector('picture');
-       
-      });
+        const li = document.createElement('li');
+        moveInstrumentation(row, li);
+        while (row.firstElementChild) li.append(row.firstElementChild);
+        [...li.children].forEach((div) => {
+            if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+            else div.className = 'cards-card-body';
+        });
+        ul.append(li);
     });
-  }
+}
