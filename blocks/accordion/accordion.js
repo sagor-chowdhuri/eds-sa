c9b2e502f97e67329165f5d20ef91714f0f5b068
SaiManikantha <ssanagavarpu@deloitte.com>
2025-03-28 12:25:51 +0530

    accordion

---
+++ blocks/accordion/accordion.js
@@ -0,0 +1,7 @@
+export default async function decorate(block) {
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Hello World123';
+    block.innerHTML = '';
+    block.append(h1);
+  }
+  
