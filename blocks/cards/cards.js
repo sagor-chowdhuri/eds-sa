4c2f36a04c7b361d5cd4da3242cbc22a18da7794
SaiManikantha <ssanagavarpu@deloitte.com>
2025-03-28 13:51:25 +0530

    .

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,24 +1,6 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
-export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
-  });
-  ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
-    img.closest('picture').replaceWith(optimizedPic);
-  });
-  block.textContent = '';
-  block.append(ul);
+export default async function decorate(block) {
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Hello World123';
+  block.innerHTML = '';
+  block.append(h1);
 }
